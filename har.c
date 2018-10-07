03b5810249c8efee40a9771bf1300f73af4729cd
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2018-10-07 18:22:40 +0530

    Add files via upload

---
+++ har.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a;
+printf("a=%d",a);
+scanf("%d",&a);
+if(a==0){
+printf("Zero");
+}
+if(a<0)
+{
+printf("negative");
+
+}
+if(a>0)
+{
+printf("positive");
+}
+return 0;
+}
