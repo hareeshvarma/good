0b8bb7c40c7f0557765f6ddde1be992fd5c07960
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2018-10-08 06:10:11 -0700

    Add files via upload

---
+++ hytr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char sub;
+    printf("enter the letter :");
+    scanf("%c",&sub);
+    if(sub=='a'||sub=='e'||sub=='i'||sub=='o'||sub=='u'||sub=='A'||sub=='E'||sub=='I'||sub=='O'||sub=='U')
+
+    }
+    else
+    {
+        printf("it is invalid input ");
+    }
+    return 0;
+}
