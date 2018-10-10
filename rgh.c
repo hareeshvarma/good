2071f4c6755ec0cc6c4a59fc098db1b34df78715
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2018-10-10 06:50:06 -0700

    Add files via upload

---
+++ rgh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int year;
+printf("Enter the year \n");
+scanf("%d",&year);
+if((year%4)== 0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+
+}
+return 0;
+}
