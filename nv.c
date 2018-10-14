cbdae78021a013a645f89462ebc13659b5958a60
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2018-10-14 06:52:13 -0700

    Add files via upload

---
+++ nv.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i, num, sum=0;
+printf("Enter the number:");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+return 0;
+}
