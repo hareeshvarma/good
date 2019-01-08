dea2255567a4e12f409c49afae697cc92c802524
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2019-01-08 23:27:16 +0530

    Add files via upload

---
+++ repititionsk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,i,k,count=0,a[100];
+scanf("%d %d",&k,&n);
+
+for(i=0;i<k;i++)
+{
+    scanf("%d",&a[i]);}
+for(i=0;i<k;i++){
+if(a[i]==n)
+    {
+        count++;}
+}
+printf("%d",count);
+return 0;
+}
