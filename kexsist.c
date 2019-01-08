f03ff2e074f0ff39e959ba39e50ce9fc7634f539
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2019-01-08 23:29:51 +0530

    Add files via upload

---
+++ kexsist.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,k,count=0,a[100];
+scanf("%d %d",&k,&n);
+
+for(i=0;i<k;i++)
+{
+    scanf("%d",&a[i]);}
+for(i=0;i<k;i++)
+{
+if(a[i]==n)
+{printf("yes");
+k=0;
+}
+}}
