a05c753a981c37ecc84d6ad75d57435cb335b159
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2019-03-13 21:13:17 +0530

    Add files via upload

--- haree.c
+++ haree.c
@@ -1,21 +1,30 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
-int a;
-printf("enter the value a:");
-scanf("%d",&a);
-if(a%2==0)
-{
-printf("even");
+  	char str[100];
+  	int i, j, k;
+
+  	printf("\n Please Enter any String :  ");
+  	gets(str);
+
+  	for(i = 0; i < strlen(str); i++)
+  	{
+  		for(j = i + 1; str[j] != '\0'; j++)
+  		{
+  			if(str[j] == str[i])
+			{
+  				for(k = j; str[k] != '\0'; k++)
+				{
+					str[k] = str[k + 1];
+				}
+ 			}
+		}
+	}
+
+	printf("\n The Final String after Removing All Duplicates = %s ", str);
+
+  	return 0;
 }
-else if(a%2==1)
-{
- printf("odd");
-}
-else
-{
-printf("invalid");
-}
-return 0;
-}
+
