888f9fd707d27140fcf2e5bda60d4554fae45be3
hareeshvarma <43881201+hareeshvarma@users.noreply.github.com>
2019-03-14 19:20:19 +0530

    Add files via upload

---
+++ ctlter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100];
+gets(s);
+puts(s);
+int count=0,i;
+for(i=0;s[i]!='\0';i++)
+{if(s[i]!=' ')
+ count++;
+}
+printf("%d",count);
+}
